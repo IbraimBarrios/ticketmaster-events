79ffe1687bd42a12c02045e83ae6e9c94f793812
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-10 14:54:05 -0600

    filtrar eventos

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -2,29 +2,41 @@ import EventItem from "./components/EventItem";
 import dataJSON from "../../data/events.json";
 import { useState } from "react";
 
-const Events = () => {
+const Events = ({ searchTerm }) => {
   const [data] = useState(dataJSON);
-  const {_embedded: { events }} = data;
+  const {
+    _embedded: { events },
+  } = data;
 
   const handleEventItemClick = (id) => {
     console.log("ticket: " + id);
   };
 
-  const eventsComponent = events.map((eventItem)=> (
-    <EventItem 
-      key={`event-item-${eventItem.id}`} 
-      name={eventItem.name} 
-      info={eventItem.info} 
-      image={eventItem.images[0].url} 
-      onEventClick={handleEventItemClick}
-      id={eventItem.id}
-    />
-  ));
+  const renderEvents = () => {
+    let eventsFiltered = events;
+
+    if (searchTerm.length > 0) {
+      eventsFiltered = eventsFiltered.filter((item) =>
+        item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
+    }
+
+    return eventsFiltered.map((eventItem) => (
+      <EventItem
+        key={`event-item-${eventItem.id}`}
+        name={eventItem.name}
+        info={eventItem.info}
+        image={eventItem.images[0].url}
+        onEventClick={handleEventItemClick}
+        id={eventItem.id}
+      />
+    ));
+  };
 
   return (
     <div>
       Eventos
-      {eventsComponent}
+      {renderEvents()}
     </div>
   );
 };
