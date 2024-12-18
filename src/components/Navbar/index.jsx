cdf54ee20a1b3c6bf37fda1f984c93cb186af2b4
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-18 16:32:43 -0600

    Como funciona useEffect

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = ({ onSearch }) => {
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    console.log("1010 useEffect...");
+  }, [search, onSearch]);
+
+  // ejemplos de useEffect
+  // sepueden tener hasta N cantidades de useEffect pero no hay que sobre pasar
+  // la cantidad de useEffect para no probocar errores
+  // useEffect(()=> {
+  //   console.log("Se monto el componente...");
+  // }, []);
+
+  // useEffect(()=> {
+  //   console.log("Search cambio...");
+  // }, [search]);
+
+  // useEffect(()=> {
+  //   console.log("onSearch Cambio...");
+  // }, [onSearch]);
 
   const handleInputChange = (evt) => {
     setSearch(evt.target.value);
