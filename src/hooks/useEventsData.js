672a51475dbe8de72b6d17558563b19f97141fbe
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-18 21:38:55 -0600

    Hook reusable

---
+++ src/hooks/useEventsData.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import eventsJSON from "../data/events.json";
+
+const useEventData = () => {
+  const [data] = useState(eventsJSON);
+  const {
+    _embedded: { events },
+  } = data;
+
+  return {
+    events,
+  };
+};
+
+export default useEventData;
