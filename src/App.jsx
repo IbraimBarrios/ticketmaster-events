16648440cfb9227d279209a7f0cf6879bc229854
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-20 13:35:38 -0600

    Creando pagina Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import { useRef, useState } from "react";
-import Navbar from "./components/Navbar";
-import Events from "./components/Events";
-import "./App.css";
-import SignupForm from "./components/SignupForm";
 import Routes from "./routers";
+import "./App.css";
 
 function App() {
-  /* 
-  const [searchTerm, setSearchTerm] = useState("");
-  const containerRef = useRef();
-
-  const handleNavbarSearch = (word) => {
-    console.log(containerRef.current.setSearch(""));
-    setSearchTerm(word);
-  };
-
-  return (
-    <>
-      <SignupForm />
-
-      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-      <Events searchTerm={searchTerm} />
-    </>
-  );
-  */
-
   return <Routes />;
 }
 
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,3 +1,22 @@
-const Home = () => <div>Home</div>;
+import { useState, useRef } from "react";
+import Navbar from "../../components/Navbar";
+import Events from "../../components/Events";
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const containerRef = useRef();
+
+  const handleNavbarSearch = (word) => {
+    console.log(containerRef.current.setSearch(""));
+    setSearchTerm(word);
+  };
+
+  return (
+    <>
+      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
+      <Events searchTerm={searchTerm} />
+    </>
+  );
+};
 
 export default Home;
