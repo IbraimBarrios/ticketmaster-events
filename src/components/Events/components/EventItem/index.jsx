950a52737844fad9309bc3d569e35d5f905df1d6
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-19 23:11:49 -0600

    Modulos de CSS

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,4 +1,4 @@
-import './style.css';
+import styles from './EventItem.module.css';
 
 const EventItem = ({info, id, name, image, onEventClick}) => {
   const handleSeeMoreClick = (evt) => {
@@ -7,12 +7,12 @@ const EventItem = ({info, id, name, image, onEventClick}) => {
   };
 
   return (
-    <div onClick={() => {console.log("Padre")}} className='event-item-container'>
+    <div onClick={() => {console.log("Padre")}} className={styles.eventItemContainer}>
       <img src={image} alt={name} width={200} height={200}/>
-      <div className='event-info-container'>
-        <h4 className='event-name'>{name}</h4>
-        <p className='event-info'>{info}</p>
-        <button onClick={handleSeeMoreClick} className='see-more-btn'>Ver más</button>
+      <div className={styles.eventInfoContainer}>
+        <h4 className={styles.eventName}>{name}</h4>
+        <p className={styles.eventInfo}>{info}</p>
+        <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>Ver más</button>
       </div>
     </div>
   );
