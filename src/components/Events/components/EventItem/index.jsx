09fab50141683622e188b658b5cded5981855488
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-19 22:53:51 -0600

    Agregar CSS con clases

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,3 +1,5 @@
+import './style.css';
+
 const EventItem = ({info, id, name, image, onEventClick}) => {
   const handleSeeMoreClick = (evt) => {
     evt.stopPropagation();
@@ -5,11 +7,13 @@ const EventItem = ({info, id, name, image, onEventClick}) => {
   };
 
   return (
-    <div onClick={() => {console.log("Padre")}}>
+    <div onClick={() => {console.log("Padre")}} className='event-item-container'>
       <img src={image} alt={name} width={200} height={200}/>
-      <h4>{name}</h4>
-      <p>{info}</p>
-      <button onClick={handleSeeMoreClick}>Ver más</button>
+      <div className='event-info-container'>
+        <h4 className='event-name'>{name}</h4>
+        <p className='event-info'>{info}</p>
+        <button onClick={handleSeeMoreClick} className='see-more-btn'>Ver más</button>
+      </div>
     </div>
   );
 };
