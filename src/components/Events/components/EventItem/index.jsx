c62e4b22ce3e9e8f1c2b465afa4da101d2848813
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-27 23:43:38 -0600

    Agregar iconos hearts

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,4 +1,6 @@
 import styles from './EventItem.module.css';
+import HeartFilled from '../../../../assets/heart-filled.png';
+import HeartUnfilled from '../../../../assets/heart-unfilled.png';
 
 const EventItem = ({info, id, name, image, onEventClick}) => {
   const handleSeeMoreClick = (evt) => {
@@ -6,9 +8,16 @@ const EventItem = ({info, id, name, image, onEventClick}) => {
     onEventClick(id)
   };
 
+  const handleHeartClick = (evt) => {
+
+  }
+
   return (
     <div onClick={() => {console.log("Padre")}} className={styles.eventItemContainer}>
-      <img src={image} alt={name} width={200} height={200}/>
+      <div className={styles.imageContainer}>
+        <img src={HeartUnfilled} alt="Heart button" className={styles.heartImage} onClick={handleHeartClick}/>
+        <img src={image} alt={name} width={200} height={200} />
+      </div>
       <div className={styles.eventInfoContainer}>
         <h4 className={styles.eventName}>{name}</h4>
         <p className={styles.eventInfo}>{info}</p>
