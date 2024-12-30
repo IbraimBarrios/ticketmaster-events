8bf8e0abb6943598a737e37e69aaece1cf14752e
Ibraim Barrios <barrios.party@hotmail.com>
2024-12-30 16:50:57 -0600

    Agregando From de mi info

--- src/views/Profile/components/MyInfo/index.jsx
+++ src/views/Profile/components/MyInfo/index.jsx
@@ -1,5 +1,72 @@
+import { useForm } from "react-hook-form";
+import styles from "./MyInfo.module.css";
+import { useEffect } from "react";
+
+const USER_DATA = "userData";
+
 const MyInfo = () => {
-  return <div>My Info</div>;
+  const { handleSubmit, register, setValue } = useForm();
+
+  const handleFromSubmit = (data) => {
+    try {
+      localStorage.setItem(USER_DATA, JSON.stringify(data));
+      alert("Usuario actualizado.");
+    } catch (error) {
+      alert("Ha ocurrido un error.");
+    }
+  };
+
+  useEffect(() => {
+    try {
+      const userData = JSON.parse(localStorage.getItem(USER_DATA)) || {};
+      setValue("name", userData?.name);
+      setValue("email", userData?.email);
+      setValue("age", userData?.age);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit(handleFromSubmit)} className={styles.form}>
+      <label className={styles.label}>
+        Name
+        <input
+          type="text"
+          {...register("name", {
+            required: true,
+            minLength: 1,
+            maxLength: 120,
+          })}
+          className={styles.input}
+        />
+      </label>
+      <label className={styles.label}>
+        Email
+        <input
+          type="text"
+          {...register("email", { required: true })}
+          className={styles.input}
+        />
+      </label>
+      <label className={styles.label}>
+        Age
+        <input
+          type="number"
+          {...register("age", {
+            required: true,
+            min: 1,
+            max: 120,
+            valueAsNumber: true,
+          })}
+          className={styles.input}
+        />
+      </label>
+      <button type="submit" className={styles.submitButton}>
+        Save
+      </button>
+    </form>
+  );
 };
 
 export default MyInfo;
